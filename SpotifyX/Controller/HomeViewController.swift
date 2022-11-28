7a99d9ecff5becc558bd3f05e072775381661668
sourav-singh-rawat <souravrawat1999@gmail.com>
2022-11-28 19:08:27 +0530

    Remove Main.storyboard from Build setting Info.plist . Add config in scene-delegate to programatically control project

---
+++ SpotifyX/Controller/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  SpotifyX
+//
+//  Created by Sourav Singh Rawat on 28/11/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
--- SpotifyX/SceneDelegate.swift
+++ SpotifyX/SceneDelegate.swift
@@ -16,7 +16,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        configureScene(withWindowScene: windowScene)
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -50,6 +53,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
+    func configureScene(withWindowScene windowScene: UIWindowScene){
+        window = UIWindow(windowScene: windowScene)
+        
+        window?.rootViewController = HomeViewController()
+        
+        window?.makeKeyAndVisible()
+    }
 
 }
 
