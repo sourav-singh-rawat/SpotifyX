8a070a0414af6803a5906fb8fc9c4c3c41c74b54
sourav-singh-rawat <souravrawat1999@gmail.com>
2022-12-01 08:18:17 +0530

    Add extension over UILabel

---
+++ SpotifyX/View/Core/CustomExtensions/LableX.swift
@@ -0,0 +1,14 @@
+//
+//  LableX.swift
+//  SpotifyX
+//
+//  Created by Sourav Singh Rawat on 01/12/22.
+//
+
+import UIKit
+
+extension UILabel {
+    func fontSize(_ size: Double){
+        self.font.withSize(size)
+    }
+}
